99ac0fb75ed221268d7b0edbb78b7e125ce12365
oharrison <onel.harrison@gmail.com>
2015-03-03 22:16:24 -0500

    Catch ValueError if string that cannot be converted to float is found in readable dataset

--- function-checker.py
+++ function-checker.py
@@ -27,29 +27,33 @@ if __name__ == "__main__":
 		relation_reader = csv.reader(csvfile, delimiter=",")
 
 		# build a relation using a dictionary
-		relation = {}
-		for row in relation_reader:
-			if float(row[0]) not in relation.keys():
-				relation[float(row[0])] = []
-				relation[float(row[0])].append(float(row[1]))
-			else:
-				if float(row[1]) not in relation[float(row[0])]:
+		try:
+			relation = {}
+			for row in relation_reader:
+				if float(row[0]) not in relation.keys():
+					relation[float(row[0])] = []
 					relation[float(row[0])].append(float(row[1]))
+				else:
+					if float(row[1]) not in relation[float(row[0])]:
+						relation[float(row[0])].append(float(row[1]))
 
 
-		# output domain and range
-		print("Domain\t\tRange")
-		for domain_element in relation.keys():
-			if len(relation[domain_element]) > 1:
-				for item in range(len(relation[domain_element])):
-					print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, range_element=relation[domain_element][item]))
+			# output domain and range
+			print("Domain\t\tRange")
+			for domain_element in relation.keys():
+				if len(relation[domain_element]) > 1:
+					for item in range(len(relation[domain_element])):
+						print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, 
+							range_element=relation[domain_element][item]))
+				else:
+					print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, range_element=relation[domain_element][0]))
+
+
+			# check if relation is a function
+			if isFunction(relation):
+				print("This relation represents a function.")
 			else:
-				print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, range_element=relation[domain_element][0]))
-
-
-		# check if relation is a function
-		if isFunction(relation):
-			print("This relation represents a function.")
-		else:
-			print("This relation does not represent a function.")
-
+				print("This relation does not represent a function.")
+		except ValueError as e:
+			print(e)
+			quit()
