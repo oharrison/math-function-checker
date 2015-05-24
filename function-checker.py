c5dd4f16b5ea041450b7f4c20cbc8c96677776ba
oharrison <onel.harrison@gmail.com>
2015-05-24 16:54:41 -0400

    refactor

--- function-checker.py
+++ function-checker.py
@@ -1,59 +1,65 @@
-#!/usr/bin/python3.4
+#!/usr/bin/env python3.4
+
+import argparse, csv
+
+def buildRelation(relation_reader):
+    relation = {}
+    for row in relation_reader:
+        if float(row[0]) not in relation.keys():
+            relation[float(row[0])] = []
+            relation[float(row[0])].append(float(row[1]))
+        else:
+            if float(row[1]) not in relation[float(row[0])]:
+                relation[float(row[0])].append(float(row[1]))
+    return relation
+
+def outputDomainAndRange(relation):
+    print("Domain\t\tRange")
+    for domain_element in relation.keys():
+        if len(relation[domain_element]) > 1:
+            for item in range(len(relation[domain_element])):
+                print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, 
+                                                    range_element=relation[domain_element][item]))
+        else:
+            print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, 
+                                                   range_element=relation[domain_element][0]))
+                                                                                           
+def isFunction(relation):
+    """
+    Returns True if parameter relation represents a function
+    and False if it does not represent a function
+    """
+    if len(relation) < 2:
+        return False
+    else:
+        for key in relation.keys():
+            if len(relation[key]) > 1:
+                return False
+    return True
+
 
 if __name__ == "__main__":
-	import argparse, csv
+    
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="read data from csv file")
+    args = parser.parse_args()
 
+    # read data from csv file
+    with open(args.filename, newline='') as csvfile:
+        relation_reader = csv.reader(csvfile, delimiter=",")
+        
+        try:
+            # build relation 
+            relation = buildRelation(relation_reader)
+            # output domain and range
+            outputDomainAndRange(relation)
 
-	def isFunction(relation):
-		"""
-		Returns True if parameter relation represents a function
-		and False if it does not represent a function
-		"""
-		if len(relation) < 2:
-			return False
-		else:
-			for key in relation.keys():
-				if len(relation[key]) > 1:
-					return False
-		return True
-
-	# parse arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument("filename", help="read data from csv file")
-	args = parser.parse_args()
-
-	# read data from csv file
-	with open(args.filename, newline='') as csvfile:
-		relation_reader = csv.reader(csvfile, delimiter=",")
-
-		# build a relation using a dictionary
-		try:
-			relation = {}
-			for row in relation_reader:
-				if float(row[0]) not in relation.keys():
-					relation[float(row[0])] = []
-					relation[float(row[0])].append(float(row[1]))
-				else:
-					if float(row[1]) not in relation[float(row[0])]:
-						relation[float(row[0])].append(float(row[1]))
-
-
-			# output domain and range
-			print("Domain\t\tRange")
-			for domain_element in relation.keys():
-				if len(relation[domain_element]) > 1:
-					for item in range(len(relation[domain_element])):
-						print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, 
-							range_element=relation[domain_element][item]))
-				else:
-					print("{domain_element}\t\t{range_element}".format(domain_element=domain_element, range_element=relation[domain_element][0]))
-
-
-			# check if relation is a function
-			if isFunction(relation):
-				print("This relation represents a function.")
-			else:
-				print("This relation does not represent a function.")
-		except ValueError as e:
-			print(e)
-			quit()
+            # check if relation is a function
+            if isFunction(relation):
+                print("This relation represents a function.")
+            else:
+                print("This relation does not represent a function.")
+        except ValueError as e:
+            print(e)
+            quit()
